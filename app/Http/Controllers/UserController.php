c81244810fe19d9432f0ef4239714771ab4dc836
IvanRizal <ivanrizal36@gmail.com>
2025-03-13 21:59:20 +0700

    praktikum 6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // Tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama'
+        ];
+
+        // Update data user
+        UserModel::where('username', 'customer-1')->update($data);
+
+        // Ambil semua data dari tabel m_user
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}

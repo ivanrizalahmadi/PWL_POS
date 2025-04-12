47f5d4eafc9b180565cc16a574465650b846331a
IvanRizal <ivanrizal36@gmail.com>
2025-04-13 01:30:41 +0700

    JS6 Praktikum 2 dan 3

--- resources/views/user/create_ajax.blade.php
+++ resources/views/user/create_ajax.blade.php
@@ -4,17 +4,16 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Tambah Data User</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
                 <div class="form-group">
                     <label>Level Pengguna</label>
                     <select name="level_id" id="level_id" class="form-control" required>
                         <option value="">- Pilih Level -</option>
-                        @foreach ($level as $item)
-                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                        @foreach ($level as $l)
+                            <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
                         @endforeach
                     </select>
                     <small id="error-level_id" class="error-text form-text text-danger"></small>
@@ -22,15 +21,18 @@
                 <div class="form-group">
                     <label>Username</label>
                     <input value="" type="text" name="username" id="username" class="form-control" required>
+
                     <small id="error-username" class="error-text form-text text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Nama</label>
                     <input value="" type="text" name="nama" id="nama" class="form-control" required>
+
                     <small id="error-nama" class="error-text form-text text-danger"></small>
                 </div>
                 <div class="form-group">
                     <label>Password</label>
+
                     <input value="" type="password" name="password" id="password" class="form-control" required>
                     <small id="error-password" class="error-text form-text text-danger"></small>
                 </div>
@@ -45,31 +47,43 @@
 
 <script>
     $(document).ready(function() {
-        // Validasi Form
         $("#form-tambah").validate({
             rules: {
-                level_id: { required: true, number: true },
-                username: { required: true, minlength: 3, maxlength: 20 },
-                nama: { required: true, minlength: 3, maxlength: 100 },
-                password: { required: true, minlength: 6, maxlength: 20 }
+                level_id: {
+                    required: true,
+                    number: true
+                },
+                username: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 20
+                },
+                nama: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 100
+                },
+                password: {
+                    required: true,
+                    minlength: 6,
+                    maxlength: 20
+                }
             },
             submitHandler: function(form) {
-                // AJAX Request saat form disubmit
                 $.ajax({
                     url: form.action,
                     type: form.method,
                     data: $(form).serialize(),
                     success: function(response) {
                         if (response.status) {
-                            $('#myModal').modal('hide'); // Menutup modal
+                            $('#myModal').modal('hide');
                             Swal.fire({
                                 icon: 'success',
                                 title: 'Berhasil',
                                 text: response.message
                             });
-                            dataUser.ajax.reload(); // Reload DataTables
+                            dataUser.ajax.reload();
                         } else {
-                            // Menampilkan error pesan
                             $('.error-text').text('');
                             $.each(response.msgField, function(prefix, val) {
                                 $('#error-' + prefix).text(val[0]);
@@ -82,7 +96,7 @@
                         }
                     }
                 });
-                return false; // Mencegah pengiriman form standar
+                return false;
             },
             errorElement: 'span',
             errorPlacement: function(error, element) {
--- routes/web.php
+++ routes/web.php
@@ -21,38 +21,26 @@ use App\Http\Controllers\SupplierController;
 Route::get('/', [WelcomeController::class, 'index']);
 
 Route::group(['prefix' => 'user'], function () {
-    // Menampilkan halaman awal user
     Route::get('/', [UserController::class, 'index']);
-
-    // Menampilkan data user dalam bentuk JSON untuk DataTables
     Route::post('/list', [UserController::class, 'list']);
-
-    // Menampilkan halaman form tambah user
     Route::get('/create', [UserController::class, 'create']);
-
-    // Menyimpan data user baru
     Route::post('/', [UserController::class, 'store']);
+    // Ajax
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']);
+    Route::post('/ajax', [UserController::class, 'store_ajax']);
 
-     //menampilkan halaman form tambah user Ajax
-     Route::get('/create_ajax', [UserController::class, 'create_ajax']);
-
-     //menyimpan data user baru ajax
-     Route::post('/ajax', [UserController::class, 'store_ajax']);
-
-    // Menampilkan detail user
     Route::get('/{id}', [UserController::class, 'show']);
-
-    // Menampilkan halaman form edit user
     Route::get('/{id}/edit', [UserController::class, 'edit']);
-
-    // Menyimpan perubahan data user
     Route::put('/{id}', [UserController::class, 'update']);
-
-    // Menghapus data user
+    // Ajax Update
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
+    // Ajax Delete
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
-
-
 });
+
 
 Route::group(['prefix' => 'kategori'], function () {
     Route::get('/', [KategoriController::class, 'index']);

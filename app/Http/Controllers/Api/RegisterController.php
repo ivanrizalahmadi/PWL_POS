17dbdda13c8032e716f018de92f3db728eccffa1
IvanRizal <ivanrizal36@gmail.com>
2025-06-16 21:51:55 +0700

    jobsheet 11 praktikum 1

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -10,47 +10,40 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-   public function __invoke(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'username' => 'required',
-        'nama' => 'required',
-        'password' => 'required|min:5|confirmed',
-        'level_id' => 'required',
-        'avatar' => 'nullable|image|mimes:jpg,jpeg,png,svg|max:2048'
-    ]);
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'nama' => 'required',
+            'password' => 'required|min:5|confirmed',
+            'level_id' => 'required',
+            'avatar' => 'required|image|mimes:jpg,jpeg,png,svg|max:2048'
+        ]);
 
-    if ($validator->fails()) {
-        return response()->json($validator->errors(), 422);
-    }
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
 
-    $avatarPath = null;
+        $avatar = $request->avatar;
 
-    if ($request->hasFile('avatar')) {
-        $avatar = $request->file('avatar');
-        $avatar->store('public/avatars'); // simpan ke storage/app/public/avatars
-        $avatarPath = $avatar->hashName(); // hanya ambil nama filenya
-    }
+        // Create User
+        $user = UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id,
+            'avatar' => $avatar->hashName()
+        ]);
 
-    // Create User
-    $user = UserModel::create([
-        'username' => $request->username,
-        'nama' => $request->nama,
-        'password' => Hash::make($request->password),
-        'level_id' => $request->level_id,
-        'avatar' => $avatarPath
-    ]);
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'user' => $user
+            ], 201);
+        }
 
-    if ($user) {
         return response()->json([
-            'success' => true,
-            'user' => $user
-        ], 201);
+            'success' => false,
+        ], 409);
     }
-
-    return response()->json([
-        'success' => false,
-    ], 409);
-}
-
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,9 @@ use App\Http\Controllers\Api\KategoriController;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
+
+
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
